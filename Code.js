a60a55fb4ab9ddd211d9c500b306bf0898d03b8a
timx1277 <timthomsen02@gmail.com>
2016-09-27 08:33:07 +0200

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,75 @@
+<html>
+
+<h1>Terninig udregner</h1>
+<h2>By: Tim</h2>
+<input type="text" id="myText" value="Indtast ønskede antal kast">
+<button onclick="myFunction()">Try it</button>
+<script>
+function myFunction(){
+var endf1 = 0;
+var endf2 = 0;
+var endf3 = 0;
+var endf4 = 0;
+var endf5 = 0;
+var endf6 = 0;
+var slagindtilnu = 0;
+var slag = document.getElementById("myText").value;;
+var a1 = 0;
+var b2 = 0;
+var c3 = 0;
+var d4 = 0;
+var e5 = 0;
+var f6 = 0;
+console.log("Udregner...");
+
+
+while(slag > slagindtilnu) {
+
+var x = Math.floor(Math.random() * 6) + 1;  
+
+switch(x){
+case 1:
+	a1++;
+	slagindtilnu++;
+	break;
+case 2:
+	b2++;
+	slagindtilnu++;
+	break;
+case 3:
+	c3++;
+	slagindtilnu++;
+	break;
+case 4:
+	d4++;
+	slagindtilnu++;
+	break;
+case 5:
+	e5++
+	slagindtilnu++;
+	break;
+case 6:
+	f6++;
+	slagindtilnu++;
+	break;
+}
+}
+endf1 = (a1/slag)*100;
+endf2 = (b2/slag)*100;
+endf3 = (c3/slag)*100;
+endf4 = (d4/slag)*100;
+endf5 = (e5/slag)*100;
+endf6 = (f6/slag)*100;
+
+console.log("Done!!");
+console.log(endf1 + " %");
+console.log(endf2 + " %");
+console.log(endf3 + " %");
+console.log(endf4 + " %");
+console.log(endf5 + " %");
+console.log(endf6 + " %");
+}
+
+
+</script>
+</html>
