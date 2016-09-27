a3f86f1fa1928fac90cadc1fb5701245f35be934
timx1277 <timthomsen02@gmail.com>
2016-09-27 08:57:51 +0200

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,9 +1,9 @@
 <html>
-
 <h1>Terninig udregner</h1>
 <h2>By: Tim</h2>
 <input type="text" id="myText" value="Indtast ønskede antal kast">
 <button onclick="myFunction()">Try it</button>
+<p id="demo"></p>
 <script>
 function myFunction(){
 var endf1 = 0;
@@ -20,9 +20,6 @@ var c3 = 0;
 var d4 = 0;
 var e5 = 0;
 var f6 = 0;
-console.log("Udregner...");
-
-
 while(slag > slagindtilnu) {
 
 var x = Math.floor(Math.random() * 6) + 1;  
@@ -53,23 +50,19 @@ case 6:
 	slagindtilnu++;
 	break;
 }
-}
-endf1 = (a1/slag)*100;
-endf2 = (b2/slag)*100;
-endf3 = (c3/slag)*100;
-endf4 = (d4/slag)*100;
-endf5 = (e5/slag)*100;
-endf6 = (f6/slag)*100;
+endf1 = (a1/slag)*100 + " %";
+endf2 = (b2/slag)*100 + " %";
+endf3 = (c3/slag)*100 + " %";
+endf4 = (d4/slag)*100 + " %";
+endf5 = (e5/slag)*100 + " %";
+endf6 = (f6/slag)*100 + " %";
 
-console.log("Done!!");
-console.log(endf1 + " %");
-console.log(endf2 + " %");
-console.log(endf3 + " %");
-console.log(endf4 + " %");
-console.log(endf5 + " %");
-console.log(endf6 + " %");
-}
+document.getElementById("demo").innerHTML =
+"Udregner..." + "<br>" +endf1 + "<br>" + endf2 + "<br>" + endf3 + "<br>" + endf4 + "<br>" + endf5 + "<br>" + endf6;
 
+
+}
+}
 
 </script>
 </html>
